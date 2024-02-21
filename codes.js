e8b4eb7ac1130d9366c3e490fc7658fe3b5f0a62
Mahdi Balali <mahdi.balali@gmail.com>
2024-02-21 15:28:30 +0330

    Point 2 of the plan is compeleted. All of the buttons are created. the memory operation buttons are created. ALso the screen is created. the window buttons are created, too.

---
+++ codes.js
@@ -0,0 +1,38 @@
+buttonsLabelArray =['%','CE','C','&#10498','1/x','x<sup>2</sup>','&#8730;x','&#247;','7','8','9','&#215;','4','5','6','-','1','2','3','+','&#177;','0','.','='];
+memOpButtonLabelArray = ['MC','MR','M+','M-','MS','M&#8744;']
+const buttonWidth = 30;
+const buttonsMargin = 1;
+const buttonsArr = [];
+const memOpButtonsArr = [];
+function createButtons(){    
+    const buttonsPane = document.querySelector('.buttons-pane');
+    index = 0;
+    for (let iRow = 1 ; iRow<=6;iRow++){
+        for (let iCol = 1;iCol<=4;iCol++){
+            buttonsArr[index] = document.createElement("button");
+            buttonsArr[index].innerHTML = buttonsLabelArray[index];
+            buttonsArr[index].id = `${iRow}-${iCol}-${buttonsLabelArray[index]}`;
+            buttonsArr[index].style.gridColumn = `${iCol}/ span 1`;
+            buttonsArr[index].style.gridRow = `${iRow}/ span 1`;
+            buttonsArr[index].classList.add('buttons');
+            buttonsPane.appendChild(buttonsArr[index]);
+            index++;
+        }
+    }
+}
+function createMemOperationButtons(){
+    const memOpbuttonsPane = document.querySelector('.memOperations-panel');
+    index = 0;
+    for (let i = 0;i<memOpButtonLabelArray.length;i++){
+        memOpButtonsArr[index] = document.createElement("button");
+        memOpButtonsArr[index].innerHTML = memOpButtonLabelArray[index];
+        memOpButtonsArr[index].id = `${i}-${memOpButtonLabelArray[index]}`;
+        
+        memOpButtonsArr[index].classList.add('memOperation-button');
+        memOpbuttonsPane.appendChild(memOpButtonsArr[index]);
+        index++;
+    }  
+}
+
+createMemOperationButtons();
+createButtons();
